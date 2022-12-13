59f33dd45d271a199321cbb72c8b4d2fc3430966
pmeinhardt <mnhrdt@gmail.com>
2022-12-13 14:36:03 +0100

    Solve day 12, part 1

---
+++ 12/part-1/src/main.rs
@@ -0,0 +1,81 @@
+extern crate pathfinding;
+
+use std::io;
+use pathfinding::prelude::bfs;
+
+static A: u32 = 'a' as u32;
+static Z: u32 = 'z' as u32;
+
+fn h(ch: char) -> u8 {
+    return ((ch as u32).clamp(A, Z) - A) as u8;
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Square(usize, usize);
+
+impl Square {
+  fn successors(&self, grid: &Vec<Vec<u8>>) -> Vec<Square> {
+    let &Square(x, y) = self;
+    let elevation = grid[y][x];
+
+    let max_x = grid[0].len();
+    let max_y = grid.len();
+
+    let mut succ: Vec<Square> = vec!();
+
+    if x > 0 && grid[y][x - 1] <= elevation + 1 {
+        succ.push(Square(x - 1, y)); // left
+    }
+
+    if x < max_x - 1 && grid[y][x + 1] <= elevation + 1 {
+        succ.push(Square(x + 1, y)); // right
+    }
+
+    if y > 0 && grid[y - 1][x] <= elevation + 1 {
+        succ.push(Square(x, y - 1)); // up
+    }
+
+    if y < max_y - 1 && grid[y + 1][x] <= elevation + 1 {
+        succ.push(Square(x, y + 1)); // down
+    }
+
+    return succ;
+  }
+}
+
+fn main() {
+    let mut grid: Vec<Vec<u8>> = Vec::new();
+
+    let mut start: Option<Square> = None;
+    let mut end: Option<Square> = None;
+
+    let stdin = io::stdin();
+
+    for (y, line) in stdin.lines().enumerate() {
+        let mut row: Vec<u8> = Vec::new();
+
+        for (x, ch) in line.unwrap().chars().enumerate() {
+            match ch {
+                'S' => {
+                    start = Some(Square(x, y));
+                    row.push(h('a'));
+                },
+                'E' => {
+                    end = Some(Square(x, y));
+                    row.push(h('z'));
+                },
+                _ => {
+                    row.push(h(ch));
+                },
+            }
+        }
+
+        grid.push(row);
+    }
+
+    let s = start.expect("no start marker");
+    let e = end.expect("no end marker");
+
+    let result = bfs(&s, |p| p.successors(&grid), |p| *p == e);
+    println!("{:?}", result.expect("no path found").len() - 1);
+}
