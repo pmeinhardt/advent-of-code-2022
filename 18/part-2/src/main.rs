c3e1de92c0b9d1626350880628602aefcd07c72c
pmeinhardt <mnhrdt@gmail.com>
2022-12-31 22:16:12 +0100

    Implement faster solution for day 18, part 2
    The new flood-fill approach takes 270 ms on average compared to 18.5 s
    for the previous solution and does not need an external crate/library.
    
    Benchmarked with `hyperfine`.

--- 18/part-2/src/main.rs
+++ 18/part-2/src/main.rs
@@ -1,10 +1,6 @@
-extern crate pathfinding;
-
 use std::cmp::{max,min};
 use std::collections::HashSet;
 use std::io;
-
-use pathfinding::prelude::astar;
 
 type Point = (i8, i8, i8); // x, y, z
 
@@ -17,12 +13,13 @@ const DIRECTIONS: [(i8, i8, i8); 6] = [
     ( 0,  0, -1),
 ];
 
-fn neighbors(p: Point) -> [Point; 6] {
+fn neighbors(p: &Point) -> [Point; 6] {
     return DIRECTIONS.map(|v| (p.0 + v.0, p.1 + v.1, p.2 + v.2));
 }
 
-fn distance(a: Point, b: Point) -> u8 {
-    return a.0.abs_diff(b.0) + a.1.abs_diff(b.1) + a.2.abs_diff(b.2);
+fn within(p: &Point, pmin: &Point, pmax: &Point) -> bool {
+    return p.0 >= pmin.0 && p.1 >= pmin.1 && p.2 >= pmin.2
+        && p.0 <= pmax.0 && p.1 <= pmax.1 && p.2 <= pmax.2;
 }
 
 fn main() {
@@ -42,7 +39,7 @@ fn main() {
         }
     }
 
-    let mut faces = cubes.len() * 6;
+    // Compute bounding box
 
     let sample = cubes.iter().next().expect("no cubes");
 
@@ -50,39 +47,45 @@ fn main() {
     let mut pmax = *sample;
 
     for c in &cubes {
-        for n in neighbors(*c) {
-            if cubes.contains(&n) {
-                faces -= 1;
-            }
-        }
-
         pmin = (min(pmin.0, c.0), min(pmin.1, c.1), min(pmin.2, c.2));
         pmax = (max(pmax.0, c.0), max(pmax.1, c.1), max(pmax.2, c.2));
     }
 
-    let outside = (pmax.0 + 1, pmax.1 + 1, pmax.2 + 1);
+    // Expand bounding box by 1 unit in each direction
 
-    for x in pmin.0..=pmax.0 {
-        for y in pmin.1..=pmax.1 {
-            for z in pmin.2..=pmax.2 {
-                let p = (x, y, z);
+    pmin = (pmin.0 - 1, pmin.1 - 1, pmin.2 - 1);
+    pmax = (pmax.0 + 1, pmax.1 + 1, pmax.2 + 1);
 
-                if cubes.contains(&p) {
-                    continue;
-                }
+    // Flood area around the body, within bounding box limits
 
-                let result = astar(&p,
-                                   |&p| neighbors(p).into_iter().filter(|n| !cubes.contains(n)).map(|p| (p, 1)),
-                                   |&p| distance(p, outside),
-                                   |&p| p == outside);
+    let mut closed: HashSet<Point> = HashSet::new();
+    let mut open: HashSet<Point> = HashSet::from([pmin, pmax]);
 
-                if result.is_none() {
-                    for n in neighbors(p) {
-                        if cubes.contains(&n) {
-                            faces -= 1;
-                        }
-                    }
-                }
+    while open.len() > 0 {
+        let mut next: HashSet<Point> = HashSet::new();
+
+        for p in open {
+            for n in neighbors(&p) {
+                if cubes.contains(&n) { continue; }
+                if closed.contains(&n) { continue; }
+                if !within(&n, &pmin, &pmax) { continue; }
+                next.insert(n);
+            }
+
+            closed.insert(p);
+        }
+
+        open = next;
+    }
+
+    // Count surfaces where flooded cubes touch cubes of the scanned body
+
+    let mut faces = 0;
+
+    for c in closed {
+        for x in neighbors(&c) {
+            if cubes.contains(&x) {
+                faces += 1;
             }
         }
     }
